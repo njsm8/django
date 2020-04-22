3a7fa4e1c37a463dd09263d8584c6f9fdce7dee1
njsm8 <njmshetty@gmail.com>
2020-04-22 07:20:54 +0530

    updated models and created base templates

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Tutorial
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("pythonprogramming.net homepage! Wow so #amaze.")
+    return render(request = request,
+    			  template_name = "main/home.html",
+    			  context = {"tutorials": Tutorial.objects.all})
