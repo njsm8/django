14d342634325dfcd2d962b6f8decfea0418a6c83
njsm8 <njmshetty@gmail.com>
2020-04-29 05:06:17 +0530

    Created models and Categories

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import NewUserForm
 
 # Create your views here.
 def homepage(request):
@@ -16,7 +17,7 @@ def homepage(request):
 
 def register(request):
 	if request.method == "POST":
-		form = UserCreationForm(request.POST)
+		form = NewUserForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			username = form.cleaned_data.get('username')
@@ -28,7 +29,7 @@ def register(request):
 			for msg in form.error_messages:
 				messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
-	form = UserCreationForm
+	form = NewUserForm
 	return render(request,
 					"main/register.html",
 					context = {"form":form})
