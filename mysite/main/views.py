d84c12a5764747e5be174d3755e52cfc2c26d851
njsm8 <njmshetty@gmail.com>
2020-04-28 20:52:22 +0530

    Added login and logout

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
@@ -32,3 +32,29 @@ def register(request):
 	return render(request,
 					"main/register.html",
 					context = {"form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "Logged out successfully")
+	return redirect("main:homepage")
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username = username,password= password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}")
+				return redirect("main:homepage")
+			else:
+				messages.error(request, "Invalid username or password")
+		else:
+			messages.error(request, "Invalide username or password")
+
+	form = AuthenticationForm()
+	return render(request,
+				   "main/login.html",
+				   {"form":form})
