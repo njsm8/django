f1298faa6dfb8c02c67b7795bea8b92c3a56ec00
njsm8 <njmshetty@gmail.com>
2020-05-03 23:25:00 +0530

    slug link

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -13,10 +13,15 @@ def single_slug(request, single_slug):
 	categories = [c.category_slug for c in TutorialCategory.objects.all()]
 	if single_slug in categories:
 		matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug= single_slug)
+		
+		series_urls = {}
 		for m in matching_series.all():
-		part_one = Tutorial.objects.filter(tutorial_series__tutorial_series)	
-		return HttpResponse(f"{single_slug} is a category!")
+			part_one = Tutorial.objects.filter(tutorial_series__tutorial_series = m.tutorial_series).earliest("tutorial_published")
+			series_urls[m] = part_one.tutorial_slug	
 
+		return render(request,
+					   "main/category.html",
+					   {"part_ones": series_urls})
 
 	tutorials = [t.tutorial_slug for c in TutorialCategory.objects.all()]
 	if single_slug in tutorials:
